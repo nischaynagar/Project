5631be41c631b1a6a5f9e1d3ea380d74c22fb2db
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-09-01 13:02:49 +0530

    google popup

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,144 +1,160 @@
-import React,{useState, useEffect} from 'react';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Signup from "./Singup";
 
 function App() {
+  const [count, setCount] = useState(0);
 
-  const [count,setCount] = useState(0)
-
-  useEffect(()=>{
+  useEffect(() => {
     axios
-    .get("https://jsonplaceholder.typicode.com/comments")
-    .then(function(response) {
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then(function (response) {
         setValue({
-          fname : response.data[count].name,
-          lname : response.data[count].email
-        })
-    })
-  },[count])
+          fname: response.data[count].name,
+          lname: response.data[count].email,
+        });
+      });
+  }, [count]);
 
+  const [value, setValue] = useState({
+    fname: "",
+    lname: "",
+  });
+  const [ide, setIde] = useState(-1);
 
+  const [list, setList] = useState([]);
 
-const [value,setValue] = useState({
-  fname : "",
-  lname: ""
-});
-const [ide,setIde] = useState(-1)
+  function increment(e) {
+    setCount(count + 1);
+    e.preventDefault();
+  }
 
-const [list,setList] = useState([])
+  function updateid(id, e) {
+    setValue(list[id]);
+    setIde(id);
 
-function increment(e){
-  setCount(count+1);
-  e.preventDefault();
-}
+    e.preventDefault();
+  }
+  function updateid1(e) {
+    setList((list) => {
+      return list.map((item, index) => {
+        if (index === ide) {
+          return value;
+        } else {
+          return item;
+        }
+      });
+    });
+    // setIde(-1);
+    // setValue({
+    //   fname : "",
+    //   lname : ""
+    // })
+    e.preventDefault();
+  }
 
+  function add(e) {
+    setList((prev) => {
+      return [...prev, value];
+    });
+    // setValue({
+    //   fname : "",
+    //   lname : ""
+    // })
 
-function updateid(id,e){
-  
-  setValue(list[id]);
-  setIde(id);
+    e.preventDefault();
+  }
 
-  e.preventDefault();
-  
-}
-function updateid1(e){
-  setList(list => {
-    return list.map((item,index) => {
-      if(index === ide){
-        return value;
-      }else{
-        return item;
-      }
-    })
-  })
-  // setIde(-1);
-  // setValue({
-  //   fname : "",
-  //   lname : ""
-  // })
-  e.preventDefault();
-}
+  function delid(id, e) {
+    setList((prev) => {
+      return list.filter((item, idx) => {
+        return idx !== id;
+      });
+    });
+  }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-
-function add(e){
-  setList(prev => {
-    return [...prev,value]
-  })
-  // setValue({
-  //   fname : "",
-  //   lname : ""
-  // })
- 
-  e.preventDefault();
-}
-
-function delid(id,e){
-  
-  setList(prev => {
-    return list.filter((item,idx) => {
-      return idx !== id
-    })
-  })
- 
-}
-
-function handleChange(event) {
-  const {name,value} = event.target;
- 
-  setValue(prev => {
-    return {
-      ...prev, 
-    [name] : value
-    }
-  })
-}
+    setValue((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
   return (
-    <div className="container">
+    // <div className="container">
+    //   <div className="jumbotron text-center">
+    //     <h4>{count}</h4>
+    //     <h4> Name: {value.fname} </h4>
+    //     <h4> email: {value.lname} </h4>
 
-    <div className="jumbotron text-center">  
+    //     <input
+    //       className="form-control w-25 mx-auto my-2"
+    //       type="text"
+    //       name="fname"
+    //       value={value.fname}
+    //       onChange={handleChange}
+    //     />
+    //     <input
+    //       className="form-control w-25 mx-auto my-2"
+    //       type="text"
+    //       name="lname"
+    //       value={value.lname}
+    //       onChange={handleChange}
+    //     />
 
-    <h4>{count}</h4>
-      <h4> Name: {value.fname}   </h4>
-      <h4> email: {value.lname}   </h4>
-    
-      
-      <input className="form-control w-25 mx-auto my-2" type="text" name="fname" value={value.fname} onChange={handleChange} />
-      <input className="form-control w-25 mx-auto my-2" type="text" name="lname" value={value.lname} onChange={handleChange}/>
-      
-      <button className="btn btn-primary mx-3 px-5 rounded-pill" onClick={add} >Add</button>
-      <button className="btn btn-primary mx-3 px-5 rounded-pill"  onClick={updateid1}>Update</button>
-      <button className="btn btn-primary mx-3 px-5" onClick={ increment} >Increment</button>
+    //     <button
+    //       className="btn btn-primary mx-3 px-5 rounded-pill"
+    //       onClick={add}
+    //     >
+    //       Add
+    //     </button>
+    //     <button
+    //       className="btn btn-primary mx-3 px-5 rounded-pill"
+    //       onClick={updateid1}
+    //     >
+    //       Update
+    //     </button>
+    //     <button className="btn btn-primary mx-3 px-5" onClick={increment}>
+    //       Increment
+    //     </button>
+    //   </div>
 
+    //   <div className="d-flex d-flex-center">
+    //     {list.map((item, index) => {
+    //       return (
+    //         <div key={index} className="jumbotron text-center p-3 w-25 m-3 ">
+    //           <h3 className="p-3">
+    //             {item.fname} {item.lname}{" "}
+    //           </h3>
 
-     
-</div>
+    //           <button
+    //             className="btn btn-primary mx-4 p-2"
+    //             onClick={(e) => {
+    //               delid(index, e);
+    //             }}
+    //           >
+    //             Delete
+    //           </button>
 
-    
-    
-   
-      <div className="d-flex d-flex-center">
-      
-{
-  
-  list.map((item, index) => {
-    return(
-     
-      <div key={index} className="jumbotron text-center p-3 w-25 m-3 " >
-     
-        <h3 className="p-3">{item.fname} {item.lname} </h3>
-        
-        
-        <button className="btn btn-primary mx-4 p-2" onClick={(e)=>{ delid(index,e)}} >Delete</button>
-  
-      <button className="btn btn-primary mx-4 p-2" onClick={(e)=>{ updateid(index,e)}}>Update</button>
-      </div>
-    )
-  })
-}
-</div>
-
+    //           <button
+    //             className="btn btn-primary mx-4 p-2"
+    //             onClick={(e) => {
+    //               updateid(index, e);
+    //             }}
+    //           >
+    //             Update
+    //           </button>
+    //         </div>
+    //       );
+    //     })}
+    //   </div>
+    // </div>
+    <div>
+      <Signup />
     </div>
   );
 }
---
+++ src/Components/Singup.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { auth } from "../firebase";
+import Firebase from "firebase/app";
+import { useHistory } from "react-router-dom";
+
+export default function Signup() {
+  const history = useHistory();
+
+  const handleGoogleLogin = (event) => {
+    event.preventDefault();
+    var provider = new Firebase.auth.GoogleAuthProvider();
+
+    auth
+      .signInWithPopup(provider)
+      .then(function (result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log(user);
+        history.push(ROUTES.HOME);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="container">
+      <div className="vertical-center">
+        <button className="btn" onClick={handleGoogleLogin}>
+          Signup with Google
+        </button>
+      </div>
+    </div>
+  );
+}
